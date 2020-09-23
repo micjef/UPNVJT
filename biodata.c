00e3d532ec94b54ab2025c3a2ac8f8a56e75e4a6
micjef <michaeljeffrysetiawan@gmail.com>
2020-09-23 16:50:33 +0700

    this is my biodata and this upload from termux using git bash formula

---
+++ biodata.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+//
+int main()
+{
+    printf("Nama\t\t\t: Michael Jeffry Setiawan\n\
+Jurusan\t\t\t: Teknik Informatika\n\
+Fakultas\t\t: Ilmu Komputer\n\
+Tempat/Tgl Lahir\t: Surabaya, 27-02-2002\n\
+Jenis kelamin\t\t: Laki-laki\n\
+Gol. Darah\t\t: O\n\
+Cita Cita \t\t : Pembisnis dan Web Developer\n\
+Hobi\t: Tidur\n\
+\t\t\t  Membuat Website\n\
+\t\t\t  Bermain musik\n\
+\t\t\t  Menonton YouTube");
+    return 0;
+}
